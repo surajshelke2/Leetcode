38b277abacdca4934523e44474ef3ebadf2ac737
suraj shelke <91287011+surajshelke2@users.noreply.github.com>
2024-11-08 14:32:03 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (9.45%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    
+    vector<int>dp;
+    int solve(int n){
+        
+        if(n==0){
+            
+            return 1;
+        }
+        
+        if(n<0) return 0 ;
+        
+        if(dp[n-1] != -1) return dp[n-1];
+        dp[n-1] = solve(n-1)+solve(n-2);
+        
+        return dp[n-1];
+    }
+    int climbStairs(int n) {
+        
+        dp.resize(n+1,-1);
+        return solve(n);
+        
+    }
+};
