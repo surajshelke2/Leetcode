e8388a31c76c4a838402a150965559fa848b88c3
suraj shelke <91287011+surajshelke2@users.noreply.github.com>
2024-11-20 00:40:32 +0530

    Time: 3 ms (70.01%), Space: 15.4 MB (45.65%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    
+    void solve(int n , int open , int close , string str , vector<string>&ans){
+        
+         if(open == n && close == n){
+             
+             ans.push_back(str);
+             return;
+         }
+        
+         if (open < n) {
+            solve(n, open + 1, close, str + '(', ans);
+        }
+        if (close < open) {
+            solve(n, open, close + 1, str + ')', ans);
+        }
+           
+    }
+    vector<string> generateParenthesis(int n) {
+        
+        vector<string>ans;
+       solve(n,0,0,"",ans);
+        
+        return ans;
+        
+    }
+};
