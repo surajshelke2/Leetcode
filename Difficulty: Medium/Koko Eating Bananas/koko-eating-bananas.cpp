2b6f09459307324d38919c2540194fe7fd802a72
suraj shelke <91287011+surajshelke2@users.noreply.github.com>
2025-02-03 08:43:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Koko Eating Bananas/koko-eating-bananas.cpp
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+  
+  
+    int get(vector<int>& arr,int perh){
+        
+        int count =0;
+        
+        
+        for(int i=0;i<arr.size();i++){
+            
+            count+= ceil((double)arr[i]/perh);
+        }
+        
+        return count;
+    }
+    int kokoEat(vector<int>& arr, int k) {
+        // Code here
+        
+        
+        int s =1;
+        
+        int e = *max_element(arr.begin(), arr.end());;
+        int ans =1;
+        while(s<=e){
+            
+            int mid = s +(e-s)/2;
+            
+            int canEat = get(arr,mid);
+            
+            
+            if( canEat > k){
+                
+                 s = mid+1;
+            }else{
+                ans = mid;
+                e = mid-1;
+            }
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        Solution ob;
+
+        cout << ob.kokoEat(arr, k);
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
